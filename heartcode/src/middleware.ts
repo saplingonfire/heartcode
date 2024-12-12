21f3dea39ffffbcda497d6a3f226aa51d5608b1a
zhi ying <17krystal24@gmail.com>
2024-12-12 10:55:30 +0800

    hihi

--- heartcode/src/middleware.ts
+++ heartcode/src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/aboutus(.*)', '/quiz(.*)', '/data(.*)'])
+const isProtectedRoute = createRouteMatcher(['/aboutus(.*)', '/quiz(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
